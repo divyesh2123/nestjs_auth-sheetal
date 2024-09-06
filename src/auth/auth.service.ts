01daa75db48adac6d133e06bf0e73dee2f6cb4eb
lenovo <lenovo@LAPTOP-BLNTEBH7>
2024-09-06 20:41:07 +0530

    changes for react

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { sign } from 'jsonwebtoken';
+import { Payload } from 'src/types/payload';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+
+    constructor(private userService: UserService) {}
+  
+    async signPayload(payload: Payload) {
+      return sign(payload, "!!ASDSDASDASDAS", { expiresIn: '7d' });
+    }
+
+    async validateUser(payload: Payload) {
+        return await this.userService.findByPayload(payload);
+      }
+
+}
